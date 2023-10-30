5cee1db39d30d6ea3d351f44050a7678b79b13da
jahnavreddy <144227880+jahnavreddy@users.noreply.github.com>
2023-10-30 14:39:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const nameContainer = document.getElementById("name-container");
+const ageContainer = document.getElementById("age-container");
+
+const myName = "Jahnavesh Reddy" 
+
+nameContainer.innerHTML = myName;
+
+var myAge = "18";
+ageContainer.innerHTML = myAge;
+
+const firstQuestion = "This is the first debugging question. If you fix this question, you will be able to see this text on your console"
+console.log(firstQuestion);
+
+const secondQuestion = "This is the second debugging question, if you fix this question you will be able to see this text on your console";
+console.log(secondQuestion);
+
+const thirdQuestion = "This is the third debugging question, if you fix this question you will be able to see this text on your console";
+console.log(thirdQuestion)
+
+const fourthQuestion = "This is the fourth debugging question, if you fix this question you will be able to see this text on your console";
+console.log(fourthQuestion)
+
+const fifthQuestion = "This is the fifth debugging question, if you fix this question you will be able to see this text on your console";
+console.log(fifthQuestion)
